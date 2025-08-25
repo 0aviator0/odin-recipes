21b730e65e3a501fcb1636a8c6dd30637c694221
Aviator <avityagi519@gmail.com>
2025-08-25 07:12:54 +0530

    add rock-paper-scissor

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -21,6 +21,7 @@ let score = {
 };
 function compare(computerChoice, userChoice){
     if (computerChoice === userChoice) {
+        alert("It's a tie! You: " + score["user"] + ", Computer: " + score["computer"]);
         return "It's a tie!";
     } else if (
         (computerChoice === "rock" && userChoice === "scissors") ||
@@ -28,24 +29,31 @@ function compare(computerChoice, userChoice){
         (computerChoice === "scissors" && userChoice === "paper")
     ) {
         score.computer++;
+        alert("Computer wins! You: " + score["user"] + ", Computer: " + score["computer"]);
         return "Computer wins!";
     } else {
         score.user++;
-        return "User wins!";
+        alert("You win! You: " + score["user"] + ", Computer: " + score["computer"]);
+        return "You win!";
     }
 }
 for(let i = 0; i < 5; i++){
+    alert("Round " + (i + 1));
     computerChoice = comChoice();
     userChoice = userInput();
     console.log("computer chose " + computerChoice + " " + "and user chose " + userChoice);
     console.log(compare(computerChoice, userChoice));
 }
-console.log("Final Score - User: " + score.user + ", Computer: " + score.computer);
+console.log("Final Score - You: " + score.user + ", Computer: " + score.computer);
 if (score.user > score.computer) {
-    console.log("User wins the game!");
+    alert("You win the game!");
+    console.log("You win the game!");
 } else if (score.user < score.computer) {
+    alert("Computer wins!");
     console.log("Computer wins the game!");
 } else {
+    alert("The game is a tie!");
     console.log("The game is a tie!");
-}
+}   
+alert("Thanks for playing!");
 console.log("Thanks for playing!");
